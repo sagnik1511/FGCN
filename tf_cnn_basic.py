6b0f9119d0b75e5a192d3367d5b44330f78f49e7
Sagnik Roy <58679327+sagnik1511@users.noreply.github.com>
2021-08-18 17:28:01 +0530

    Update tf_cnn_basic.py

--- tf_cnn_basic.py
+++ tf_cnn_basic.py
@@ -1,87 +1,75 @@
 import tensorflow as tf
+import tensorflow.keras as keras
+import keras.layers as layers
 
 
-def BN(data, bn_momentum=0.9, name=None):
-    return tf.compat.v1.layers.batch_normalization(data, momentum=bn_momentum, name=None)
+def BN(data, bn_momentum=0.9,name=None):
 
+    return layers.BatchNormalization(momentum = bn_momentum, name = None)(data)
 
-def AC(data, name=None):
-    return tf.nn.relu(data, name=None)
+def AC(data,name=None):
 
+    return layers.ReLU(data)
 
-def BN_AC(data, momentum=0.9, name=None):
-    bn = BN(data=data, name=None)
-    bn_ac = AC(data=bn, name=None)
+def BN_AC(data, momentum=0.9,name=None):
+    bn = BN(data, bn_momentum = momentum)
+    bn_ac = AC(bn)
+
     return bn_ac
 
+def Conv(data, num_filter, kernel, stride = (1, 1), pad = 'valid', name = None, no_bias = None, w =  None, b = None, attr = None, num_groups = 1):
+    if w is None :
+        conv = layers.Conv2D(filters = num_filter, kernel_size = kernel, stride = stride, padding = pad, name = name, use_bias = no_bias)(data)
 
-def Conv(data, num_filter, kernel, stride=(1, 1), pad='valid', name=None, no_bias=False, w=None, b=None, attr=None,
-         num_group=1):
-    if w is None:
-        conv = tf.compat.v1.layers.conv2d(inputs=data, filters=num_filter, kernel_size=kernel,
-                                strides=stride, padding=pad, name=None, use_bias=no_bias)
+    elif b is None :
+        conv = layers.Conv2D(filters = num_filter, kernel_size = kernel, stride = stride, padding = pad, name = name, use_bias = no_bias, kernel_initializer = w)(data)
     else:
-        if b is None:
-            conv = tf.compat.v1.layers.conv2d(data=data, num_filter=num_filter, kernel_size=kernel,
-                                    stride=stride, padding=pad, name=None, use_bias=no_bias,
-                                    kernel_initializer=w)
-        else:
-            conv = tf.compat.v1.layers.conv2d(data=data, num_filter=num_filter, kernel_size=kernel,
-                                    stride=stride, padding=pad, name=None, use_bias=True,
-                                    kernel_initializer=w, bias_initializer=b)
+        conv = layers.Conv2D(filters = num_filter, kernel_size = kernel, stride = stride, padding = pad, name = name,
+                             use_bias = no_bias, kernel_initializer = b)(data)
     return conv
 
+def Conv_BN(data, num_filter, kernel, stride = (1, 1), pad = 'valid', name = None, no_bias = None, w = None, b = None, attr = None, num_groups = 1):
+    cov = Conv(data, num_filter, kernel, stride = (1, 1), pad = 'valid', name = None, no_bias = None, w = None, b = None, attr = None, num_groups = 1)
+    conv_bn = BN(cov)
 
-# - - - - - - - - - - - - - - - - - - - - - - -
-# Standard Common functions < CVPR >
-def Conv_BN(data, num_filter, kernel, pad, stride=(1, 1), name=None, w=None, b=None, no_bias=False, attr=None,
-            num_group=1):
-    cov = Conv(data=data, num_filter=num_filter, num_group=num_group, kernel=kernel, pad=pad, stride=stride, name=name,
-               w=w, b=b, no_bias=no_bias, attr=attr)
-    cov_bn = BN(data=cov, name=None)
-    return cov_bn
+    return conv_bn
 
+def Conv_BN_AC(data, num_filter, kernel, stride = (1, 1), pad = 'valid', name = None, no_bias = None, w = None, b = None, attr = None, num_groups = 1):
+    conv_bn = Conv_BN(data, num_filter, kernel, stride = (1, 1), pad = 'valid', name = None, no_bias = None, w = None, b = None, attr = None, num_groups = 1)
+    conv_bn_ac = AC(conv_bn)
 
-def Conv_BN_AC(data, num_filter, kernel, pad, stride=(1, 1), name=None, w=None, b=None, no_bias=False, attr=None,
-               num_group=1):
-    cov_bn = Conv_BN(data=data, num_filter=num_filter, num_group=num_group, kernel=kernel, pad=pad, stride=stride,
-                     name=name, w=w, b=b, no_bias=no_bias, attr=attr)
-    cov_ba = AC(data=cov_bn, name=None)
-    return cov_ba
+    return conv_bn_ac
 
+def BN_Conv(data, num_filter, kernel, stride = (1, 1), pad = 'valid', name = None, no_bias = None, w = None, b = None, attr = None, num_groups = 1):
+    bn = BN(data, num_filter, kernel, stride = (1, 1), pad = 'valid', name = None, no_bias = None, w = None, b = None, attr = None, num_groups = 1)
+    bn_conv = Conv(bn, num_filter, kernel, stride= (1, 1), pad = 'valid', name = None, no_bias = None, w = None, b = None, attr = None, num_groups = 1)
 
-# - - - - - - - - - - - - - - - - - - - - - - -
-# Standard Common functions < ECCV >
-def BN_Conv(data, num_filter, kernel, pad, stride=(1, 1), name=None, w=None, b=None, no_bias=False, attr=None,
-            num_group=1):
-    bn = BN(data=data, name=None)
-    bn_cov = Conv(data=bn, num_filter=num_filter, num_group=num_group, kernel=kernel, pad=pad, stride=stride, name=None,
-                  w=w, b=b, no_bias=no_bias, attr=attr)
-    return bn_cov
+    return bn_conv
 
-def AC_Conv(data, num_filter, kernel, pad, stride=(1, 1), name=None, w=None, b=None, no_bias=False, attr=None,
-            num_group=1):
-    ac = AC(data=data, name=None)
-    ac_cov = Conv(data=ac, num_filter=num_filter, num_group=num_group, kernel=kernel, pad=pad, stride=stride, name=None,
-                  w=w, b=b, no_bias=no_bias, attr=attr)
-    return ac_cov
+def AC_Conv(data, num_filter, kernel, stride = (1, 1), pad = 'valid', name = None, no_bias = None, w = None, b = None, attr = None, num_groups = 1):
+    ac = AC(data = data)
+    ac_conv = Conv(ac, num_filter, kernel, stride = (1, 1), pad = 'valid', name = None, no_bias = None, w = None, b = None, attr = None, num_groups = 1)
+
+    return ac_conv
+
+def BN_AC_Conv(data, num_filter, kernel, stride = (1, 1), pad = 'valid', name = None, no_bias = None, w = None, b = None, attr = None, num_groups = 1):
+    bn = BN(data = data)
+    bn_ac_conv = AC_Conv(bn, num_filter, kernel, stride = (1, 1), pad = 'valid', name = None, no_bias = None, w = None, b = None, attr = None, num_groups = 1)
 
 
-def BN_AC_Conv(data, num_filter, kernel, pad, stride=(1, 1), name=None, w=None, b=None, no_bias=False, attr=None,
-               num_group=1):
-    bn = BN(data=data, name=None)
-    ba_cov = AC_Conv(data=bn, num_filter=num_filter, num_group=num_group, kernel=kernel, pad=pad, stride=stride,
-                     name=name, w=w, b=b, no_bias=no_bias, attr=attr)
-    return ba_cov
+    return bn_ac_conv
 
-def Pooling(data, pool_type='avg', kernel=(2, 2),pad='valid', stride=(2, 2), name=None):
-    if pool_type == 'avg':
-        return tf.compat.v1.layers.average_pooling2d(inputs=data, pool_size=kernel, strides=stride, padding=pad, name=None)
-    elif pool_type == 'max':
-        return tf.compat.v1.layers.max_pooling2d(inputs=data, pool_size=kernel, strides=stride, padding=pad, name=None)
+def Pooling(data, pool_type = 'avg', kernel = (2, 2), pad = 'valid', stride = (2, 2), name = None):
+    if pool_type=='avg':
+        return layers.AveragePooling2D(pool_size = kernel, stride = stride, padding = pad, name = name)(data)
+    else:
+        return Layers.MaxPooling2D(pool_size = kernel, stride = stride, padding = pad, name = name)(data)
 
-def ElementWiseSum(x, y, name=None):
-    return tf.add(x=x, y=y, name=None)
+def ElementWiseSum(x,y,name=None):
+    return tf.add(x = x, y = y, name = None)
 
-def UpSampling(lf_conv, scale=2, sample_type='nearest',num_args=1, name=None):
-    return tf.keras.layers.UpSampling2D(size=(scale, scale), name=None)(lf_conv)
+def Upsampling(lf_data, scale=2,sample_type='nearest',num_args=1,name=None):
+    return layers.UpSampling2D(size = (scale, scale), name = None)(lf_data)
+
+
+
